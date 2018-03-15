da7f63416dfd614cbde4feb15bb44477fcc5554b
Łukasz Paczos <lukas.paczos@gmail.com>
2018-03-15 13:10:07 +0100

    pointers management tests

---
+++ library/src/test/java/com/mapbox/android/gestures/PointersManagementTest.java
@@ -0,0 +1,154 @@
+package com.mapbox.android.gestures;
+
+import android.view.MotionEvent;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static com.mapbox.android.gestures.TestUtils.getMotionEvent;
+import static org.mockito.Mockito.spy;
+
+@RunWith(RobolectricTestRunner.class)
+public class PointersManagementTest extends
+  AbstractGestureDetectorTest<StandardScaleGestureDetector,
+    StandardScaleGestureDetector.StandardOnScaleGestureListener> {
+
+  @Override
+  StandardScaleGestureDetector getDetectorObject() {
+    return spy(androidGesturesManager.getStandardScaleGestureDetector());
+  }
+
+  private void checkResult(int expected) {
+    int pointersCount = androidGesturesManager.getStandardScaleGestureDetector().getPointersCount();
+    Assert.assertTrue(
+      String.format("Expected %d pointers, was %d.", expected, pointersCount),
+      pointersCount == expected
+    );
+  }
+
+  @Test
+  public void missingDownTest() {
+    MotionEvent pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+
+    checkResult(0);
+  }
+
+  @Test
+  public void missingUpTest() {
+    MotionEvent downEvent = getMotionEvent(MotionEvent.ACTION_DOWN, 0, 0);
+    androidGesturesManager.onTouchEvent(downEvent);
+
+    downEvent = getMotionEvent(MotionEvent.ACTION_DOWN, 0, 0, downEvent);
+    androidGesturesManager.onTouchEvent(downEvent);
+
+    checkResult(1);
+  }
+
+  @Test
+  public void missingPointerDownTest() {
+    MotionEvent downEvent = getMotionEvent(MotionEvent.ACTION_DOWN, 0, 0);
+    androidGesturesManager.onTouchEvent(downEvent);
+
+    MotionEvent pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0, downEvent);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+
+    MotionEvent pointerUpEvent = getMotionEvent(MotionEvent.ACTION_POINTER_UP, 0, 0, pointerDownEvent);
+    androidGesturesManager.onTouchEvent(pointerUpEvent);
+
+    pointerUpEvent = getMotionEvent(MotionEvent.ACTION_POINTER_UP, 0, 0, pointerUpEvent);
+    androidGesturesManager.onTouchEvent(pointerUpEvent);
+
+    pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0, pointerUpEvent);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+
+    checkResult(0); //expecting 0, because we are waiting for ACTION_DOWN to synchronise again
+  }
+
+  @Test
+  public void missingPointerUpTest() {
+    MotionEvent downEvent = getMotionEvent(MotionEvent.ACTION_DOWN, 0, 0);
+    androidGesturesManager.onTouchEvent(downEvent);
+
+    MotionEvent pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0, downEvent);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+
+    pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0, pointerDownEvent);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+
+    MotionEvent pointerUpEvent = getMotionEvent(MotionEvent.ACTION_POINTER_UP, 0, 0, pointerDownEvent);
+    androidGesturesManager.onTouchEvent(pointerUpEvent);
+
+    MotionEvent upEvent = getMotionEvent(MotionEvent.ACTION_UP, 0, 0, pointerUpEvent);
+    androidGesturesManager.onTouchEvent(upEvent);
+
+    checkResult(0);
+  }
+
+  @Test
+  public void addingRemovingPointersTest() {
+    MotionEvent downEvent = getMotionEvent(MotionEvent.ACTION_DOWN, 0, 0);
+    androidGesturesManager.onTouchEvent(downEvent);
+
+    MotionEvent pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0, downEvent);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+
+    pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0, pointerDownEvent);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+
+    pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0, pointerDownEvent);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+
+    checkResult(4);
+
+    MotionEvent pointerUpEvent = getMotionEvent(MotionEvent.ACTION_POINTER_UP, 0, 0, pointerDownEvent);
+    androidGesturesManager.onTouchEvent(pointerUpEvent);
+
+    pointerUpEvent = getMotionEvent(MotionEvent.ACTION_POINTER_UP, 0, 0, pointerUpEvent);
+    androidGesturesManager.onTouchEvent(pointerUpEvent);
+
+    pointerUpEvent = getMotionEvent(MotionEvent.ACTION_POINTER_UP, 0, 0, pointerUpEvent);
+    androidGesturesManager.onTouchEvent(pointerUpEvent);
+
+    MotionEvent upEvent = getMotionEvent(MotionEvent.ACTION_UP, 0, 0, pointerUpEvent);
+    androidGesturesManager.onTouchEvent(upEvent);
+
+    checkResult(0);
+  }
+
+  // TODO: 15.03.18 fix this test
+  /*@Test
+  public void stopAnalysisWhenPointersMisalignedTest() {
+    MotionEvent downEvent = getMotionEvent(MotionEvent.ACTION_DOWN, 0, 0);
+    androidGesturesManager.onTouchEvent(downEvent);
+
+    MotionEvent pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0, downEvent);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+
+    pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0, pointerDownEvent);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+
+    pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0, pointerDownEvent);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+
+    MotionEvent pointerUpEvent = getMotionEvent(MotionEvent.ACTION_POINTER_UP, 0, 0, pointerDownEvent);
+    androidGesturesManager.onTouchEvent(pointerUpEvent);
+
+    MotionEvent upEvent = getMotionEvent(MotionEvent.ACTION_UP, 0, 0, pointerUpEvent);
+    androidGesturesManager.onTouchEvent(upEvent);
+
+    // don't analyze when events out of sync
+    doReturn(true).when(gestureDetector).checkPressure();
+    doReturn(false).when(gestureDetector).isSloppyGesture();
+
+    MotionEvent moveEvent = getMotionEvent(MotionEvent.ACTION_MOVE, 0, 0, upEvent);
+    androidGesturesManager.onTouchEvent(moveEvent);
+
+    verify(gestureDetector, times(0)).analyzeMovement();
+
+    pointerDownEvent = getMotionEvent(MotionEvent.ACTION_POINTER_DOWN, 0, 0, pointerUpEvent);
+    androidGesturesManager.onTouchEvent(pointerDownEvent);
+  }*/
+}
