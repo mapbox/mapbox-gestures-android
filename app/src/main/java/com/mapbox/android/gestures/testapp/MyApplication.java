9011ccef41ab2c8ba5e69c78f1e10c0da19bcf03
Łukasz Paczos <lukas.paczos@gmail.com>
2018-03-26 12:46:35 +0200

    moved Mapbox initialisation to the activity

--- app/src/main/java/com/mapbox/android/gestures/testapp/MyApplication.java
+++ app/src/main/java/com/mapbox/android/gestures/testapp/MyApplication.java
@@ -1,19 +1,10 @@
 package com.mapbox.android.gestures.testapp;
 
 import android.app.Application;
-import android.text.TextUtils;
-
-import com.mapbox.mapboxsdk.Mapbox;
 
 import timber.log.Timber;
 
 public class MyApplication extends Application {
-
-  private static final String DEFAULT_MAPBOX_ACCESS_TOKEN = "YOUR_MAPBOX_ACCESS_TOKEN_GOES_HERE";
-  private static final String ACCESS_TOKEN_NOT_SET_MESSAGE = "In order to run the Test App you need to set a valid "
-    + "access token. During development, you can set the MAPBOX_ACCESS_TOKEN environment variable for the SDK to "
-    + "automatically include it in the Test App. Otherwise, you can manually include it in the "
-    + "res/values/developer-config.xml file in the MapboxGLAndroidSDKTestApp folder.";
 
   @Override
   public void onCreate() {
@@ -22,12 +13,5 @@ public class MyApplication extends Application {
     if (BuildConfig.DEBUG) {
       Timber.plant(new Timber.DebugTree());
     }
-
-    String mapboxAccessToken = Utils.getMapboxAccessToken(getApplicationContext());
-    if (TextUtils.isEmpty(mapboxAccessToken) || mapboxAccessToken.equals(DEFAULT_MAPBOX_ACCESS_TOKEN)) {
-      Timber.e(ACCESS_TOKEN_NOT_SET_MESSAGE);
-    }
-
-    Mapbox.getInstance(getApplicationContext(), mapboxAccessToken);
   }
 }
