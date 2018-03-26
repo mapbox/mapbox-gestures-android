f91285875511e355c01cd6fe742957c81cc318d9
Łukasz Paczos <lukas.paczos@gmail.com>
2018-03-26 12:26:47 +0200

    added MapboxActivity

---
+++ app/src/main/java/com/mapbox/android/gestures/testapp/MapboxActivity.java
@@ -0,0 +1,65 @@
+package com.mapbox.android.gestures.testapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mapbox.mapboxsdk.maps.MapView;
+
+/**
+ * Test activity showcasing a simple MapView with current mapbox-gestures-android library commit.
+ */
+public class MapboxActivity extends AppCompatActivity {
+
+  private MapView mapView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_mapbox);
+
+    mapView = (MapView) findViewById(R.id.map_view);
+    mapView.onCreate(savedInstanceState);
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    mapView.onStart();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mapView.onResume();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    mapView.onPause();
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    mapView.onStop();
+  }
+
+  @Override
+  public void onLowMemory() {
+    super.onLowMemory();
+    mapView.onLowMemory();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    mapView.onDestroy();
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    mapView.onSaveInstanceState(outState);
+  }
+}
