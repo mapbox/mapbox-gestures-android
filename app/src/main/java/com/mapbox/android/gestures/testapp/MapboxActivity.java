51d38fd382950e4112f9c7768ac93000db218577
yunik <dmitry.yunitski@mapbox.com>
2022-08-18 16:40:07 +0300

    Bump Maps to v10.

--- app/src/main/java/com/mapbox/android/gestures/testapp/MapboxActivity.java
+++ app/src/main/java/com/mapbox/android/gestures/testapp/MapboxActivity.java
@@ -4,9 +4,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.text.TextUtils;
 
-import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
 
 import timber.log.Timber;
 
@@ -22,8 +21,6 @@ public class MapboxActivity extends AppCompatActivity {
       + "automatically include it in the Test App. Otherwise, you can manually include it in the "
       + "res/values/developer-config.xml file in the mapbox-gestures-android/app folder.";
 
-  private MapView mapView;
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -33,54 +30,9 @@ public class MapboxActivity extends AppCompatActivity {
       Timber.e(ACCESS_TOKEN_NOT_SET_MESSAGE);
     }
 
-    Mapbox.getInstance(getApplicationContext(), mapboxAccessToken);
-
     setContentView(R.layout.activity_mapbox);
 
-    mapView = (MapView) findViewById(R.id.map_view);
-    mapView.onCreate(savedInstanceState);
-    mapView.getMapAsync(mapboxMap -> mapboxMap.setStyle(Style.MAPBOX_STREETS));
-  }
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-    mapView.onStart();
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-    mapView.onResume();
-  }
-
-  @Override
-  protected void onPause() {
-    super.onPause();
-    mapView.onPause();
-  }
-
-  @Override
-  protected void onStop() {
-    super.onStop();
-    mapView.onStop();
-  }
-
-  @Override
-  public void onLowMemory() {
-    super.onLowMemory();
-    mapView.onLowMemory();
-  }
-
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    mapView.onDestroy();
-  }
-
-  @Override
-  protected void onSaveInstanceState(Bundle outState) {
-    super.onSaveInstanceState(outState);
-    mapView.onSaveInstanceState(outState);
+    MapView mapView = (MapView) findViewById(R.id.map_view);
+    mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
   }
 }
