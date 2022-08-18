aac48fba183afc8cd5356308e1ea64e0d9d44627
yunik <dmitry.yunitski@mapbox.com>
2022-08-18 16:44:01 +0300

    Remove wrong NonNull annotations from SimpleStandardOnGestureListener.

--- library/src/main/java/com/mapbox/android/gestures/StandardGestureDetector.java
+++ library/src/main/java/com/mapbox/android/gestures/StandardGestureDetector.java
@@ -97,47 +97,47 @@ public class StandardGestureDetector extends BaseGesture<StandardGestureDetector
    */
   public static class SimpleStandardOnGestureListener implements StandardOnGestureListener {
     @Override
-    public boolean onSingleTapConfirmed(@NonNull MotionEvent e) {
+    public boolean onSingleTapConfirmed(MotionEvent e) {
       return false;
     }
 
     @Override
-    public boolean onDoubleTap(@NonNull MotionEvent e) {
+    public boolean onDoubleTap(MotionEvent e) {
       return false;
     }
 
     @Override
-    public boolean onDoubleTapEvent(@NonNull MotionEvent e) {
+    public boolean onDoubleTapEvent(MotionEvent e) {
       return false;
     }
 
     @Override
-    public boolean onDown(@NonNull MotionEvent e) {
+    public boolean onDown(MotionEvent e) {
       return false;
     }
 
     @Override
-    public void onShowPress(@NonNull MotionEvent e) {
+    public void onShowPress(MotionEvent e) {
 
     }
 
     @Override
-    public boolean onSingleTapUp(@NonNull MotionEvent e) {
+    public boolean onSingleTapUp(MotionEvent e) {
       return false;
     }
 
     @Override
-    public boolean onScroll(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
       return false;
     }
 
     @Override
-    public void onLongPress(@NonNull MotionEvent e) {
+    public void onLongPress(MotionEvent e) {
 
     }
 
     @Override
-    public boolean onFling(@NonNull MotionEvent e1, @NonNull  MotionEvent e2, float velocityX, float velocityY) {
+    public boolean onFling(MotionEvent e1,  MotionEvent e2, float velocityX, float velocityY) {
       return false;
     }
   }
